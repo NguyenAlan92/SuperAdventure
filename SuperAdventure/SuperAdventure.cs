a361d72a91d190a0c4e95a883d5ed2bc3a67c238
NguyenAlan92 <alan.nguyen92@gmail.com>
2017-01-29 19:06:00 -0800

    Completed "Saving and loading player information with XML".

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -8,23 +8,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
         private Player _player;
-
         private Monster _currentMonster;
+        private const string PLAYER_DATA_FILE_NAME = "PlayerData.xml";
 
         public SuperAdventure()
         {
             InitializeComponent();
 
-            _player = new Player(20, 0, 10, 10);
-            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            if (File.Exists(PLAYER_DATA_FILE_NAME))
+            {
+                _player = Player.CreatePlayerFromXmlString(File.ReadAllText(PLAYER_DATA_FILE_NAME));
+            }
+            else
+            {
+                _player = Player.CreateDefaultPlayer();            
+            }
 
+            MoveTo(_player.CurrentLocation);
             UpdatePlayerStats();
         }
 
@@ -483,6 +490,11 @@ namespace SuperAdventure
             lblGold.Text = _player.Gold.ToString();
             lblHitPoints.Text = _player.CurrentHitPoints.ToString();
             lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void SuperAdventure_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(PLAYER_DATA_FILE_NAME, _player.ToXmlString());
         }
     }
 }
